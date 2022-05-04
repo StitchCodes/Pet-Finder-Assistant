8ff9b87dacc2a66f48eed398a5437b8f42548dde
ernest1589 <87840643+ernest1589@users.noreply.github.com>
2022-05-03 23:06:35 -0500

    add pet file and utils auth

--- client/pages/addPet.js
+++ client/pages/addPet.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 const addPet = () => {
-  return <div>addPet</div>;
+  return (
+    <div>
+      <div>Picture of the Search with bunch of animals</div>
+      <div>
+        <form>Add Petform</form>
+      </div>
+    </div>
+  );
 };
 
 export default addPet;
