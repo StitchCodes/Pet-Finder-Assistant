a9762e62add84e7435ae5a8f7604a7bb57951507
Mario Patiño <mariopatino@users.noreply.github.com>
2022-05-03 21:00:02 -0500

    Adding User files

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,20 +3,28 @@ const { gql } = require('apollo-server-express');
 
 // TODO: Create full model
 const typeDefs = gql `
-    type Category {
+    type User {
         _id: ID
+        email: String
+        nickname: String
         name: String
+        lastname: String
+        phone: String
+        password: String
+  }
+
+    type Auth {
+        token: ID!
+        user: User
     }
     
     type Query { 
-        categories: [Category]
+        user(email: String!, password: String!): User
     }
 
     type Mutation {
-        addPost
-        removePost
-        createUser
-        login
+        addUser(email: String!, nickname: String, name:String!, lastname:String, phone: String!, password: String!): Auth
+        login(email: String!, password: String!): Auth
     }
     `;
 
