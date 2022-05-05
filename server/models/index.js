470ce7ae53348fd827f2f6096dd588b534bf9565
Mario Patiño <mariopatino@users.noreply.github.com>
2022-05-04 20:09:13 -0500

    Updating and adding models

---
+++ server/models/index.js
@@ -0,0 +1,5 @@
+const User = require('./User');
+const Placard = require('./Placard');
+const Pet = require('./Pet');
+
+module.exports = { User, Placard, Pet };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,73 +1,63 @@
-// CREATE GRAPHQL USAGE
 const { gql } = require('apollo-server-express');
 
-// TODO: Create full model
-const typeDefs = gql `
-    type User {
-        _id: ID
-        email: String
-        nickname: String
-        name: String
-        lastname: String
-        phone: String
-        password: String
-  }
+const typeDefs = gql
+`
+type User {
+    _id: ID
+    email: String
+    nickname: String
+    name: String
+    lastname: String
+    phone: String
+    password: String
+    Placard: [Placard]
+}
 
-    type Auth {
-        token: ID!
-        user: User
-    }
-    
-    type pet {
-        _id: ID
-        petName: String
-        petSpecies: String
-        petGender: String
-        petColor: String
-        petDesc: String
-        petDateLF: Date
-        petStatus: Boolean
-        petPhoto: String
-        petReward: Number
-        petComments: [Posts]
-    }
+type Auth {
+    token: ID!
+    user: User
+}
 
-    type Placard {
-        _id: ID
-        placardText: String
-        placardAuthor: String
-        createdAt: String
-        placardPet: [ Pet ]
-        location
-        comments: [Comment]
-    }
+type Pet {
+    _id: ID
+    petName: String
+    petSpecies: String
+    petGender: String
+    petColor: String
+    petDesc: String
+    petDateLF: String
+    petStatus: String
+    petPhoto: String
+    petReward: Int
+}
 
-    type Comment {
-        _id: ID
-        commentText: String
-        commentAuthor: String
-        createdAt: String
-    }
+type Placard {
+    _id: ID
+    placardText: String
+    placardAuthor: String
+    createdAt: String
+    placardPet: Pet
+    comments: [Comment]
+}
 
-    type Auth {
-        token: ID!
-        user: User
-    }
+type Comment {
+    _id: ID
+    commentText: String
+    commentAuthor: String
+    createdAt: String
+}
 
-    type Query { 
-        user(email: String!, password: String!): User
-    }
-      
-    type Mutation {
-        addUser(email: String!, nickname: String, name:String!, lastname:String, phone: String!, password: String!): Auth
-        updateUser(nickname: String, name:String!, lastname:String, phone: String!, password: String!): Auth
-        login(email: String!, password: String!): Auth
-        addPlacard(placardText: String!): Placard
-        removePlacard(PlacardId: ID!): Placard
-        addComment(Placard: ID, commentText: String!): Placard
-    }
-    `;
-
-
-
-module.export = typeDefs;
+type Query { 
+    user(email: String!, password: String!): User
+}
+  
+type Mutation {
+    addUser(email: String!, nickname: String, name:String!, lastname:String, phone: String!, password: String!): Auth
+    updateUser(nickname: String, name:String!, lastname:String, phone: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addPlacard(placardText: String!): Placard
+    removePlacard(PlacardId: ID!): Placard
+    addComment(Placard: ID, commentText: String!): Placard
+}
+`;
+module.exports = typeDefs;
