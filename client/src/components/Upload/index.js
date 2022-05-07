fcce25e40b781eaebbc3cbf5173ac2889e68040b
ernest1589 <87840643+ernest1589@users.noreply.github.com>
2022-05-07 17:31:15 -0500

    eslint cache

--- client/src/components/Upload/index.js
+++ client/src/components/Upload/index.js
@@ -1,9 +1,20 @@
 import React from "react";
+import Axios from "axios";
 
 const Upload = () => {
+  const uploadImage = (files) => {
+    const formData = new FormData();
+    formData.append("file", files[0]);
+  };
+
   return (
     <div>
-      <input type="file" />
+      <input
+        type="file"
+        onChange={(event) => {
+          uploadImage(event.target.files);
+        }}
+      />
     </div>
   );
 };
