dbbf031124ab3358a23fec45e2b5727f3ffa746d
ernest1589 <87840643+ernest1589@users.noreply.github.com>
2022-05-06 22:58:15 -0500

    make Image work on footer

--- client/src/components/footer/index.js
+++ client/src/components/footer/index.js
@@ -1,17 +1,19 @@
-import React from 'react'
-import { Segment, Container, Image } from 'semantic-ui-react'
-
+import React from "react";
+import { Segment, Container, Image } from "semantic-ui-react";
+import Logo from "../../assets/images/logo.png";
 const Footer = () => {
-    return (
-        <Segment fixed='bottom' inverted color='teal'>
-            <Container textAlign='center'>
-            <div>
-                Pawtroller: Your Pet Finder Assistant  
-            </div>
-            </Container>
-         </Segment>
-    );
+  return (
+    <Segment fixed="bottom" inverted color="teal">
+      <Container textAlign="center">
+        <Image
+          height="30"
+          src={Logo}
+          style={{ margin: "0", padding: "0px" }}
+        ></Image>
+        <div>Pawtroller: Your Pet Finder Assistant</div>
+      </Container>
+    </Segment>
+  );
 };
-        
+
 export default Footer;
-        
