4885e9637c9ceeae2f43bcb7f18e58fa21ef8f36
Mario Patiño <mariopatino@users.noreply.github.com>
2022-05-10 16:42:18 -0500

    individualPlacard first version

--- client/src/pages/IndividualPlacard.js
+++ client/src/pages/IndividualPlacard.js
@@ -7,17 +7,41 @@ import {
   Image,
   Segment,
 } from 'semantic-ui-react';
+
+
+import { useQuery } from '@apollo/client';
+import { SINGLE_PLACARD_QUERY } from '../utils/queries';
+
+
 import CommentSection from '../components/CommentSection';
 import MapComponent from '../components/MapComponent';
 import EmailModal from '../components/EmailModal';
-import Dog1 from "../assets/images/dog3.png";
+//import Dog1 from "../assets/images/dog3.png";
+import { useParams } from 'react-router-dom';
 
-const IndividualPlacard = () => (
+function IndividualPlacard() {
+  // Use `useParams()` to retrieve value of the route parameter `:placardId`
+  // const { placardId } = useParams(); 
+  let petName; 
+  const { placardId } = {placardId: "6276029ae62f7a036794c8ab"}
+  const { data } = useQuery(SINGLE_PLACARD_QUERY, {
+    variables: {placardId: placardId},
+  });
+  if (data) {
+    let petName = data.petName;
+  }
+  
+  console.log(petName);
+  console.log({placardId});
+  console.log({data});
+
+return (
+<>
   <div>
     <Container style={{ marginTop: '6em' }}>
       <Grid>
 
-        <Grid.Row>
+      <Grid.Row>
            <Grid.Column floated='right' width={5}>
            </Grid.Column>
         </Grid.Row>
@@ -27,28 +51,28 @@ const IndividualPlacard = () => (
           <Card fluid>
               <Card.Content>
               <Grid.Column floated='left' width={3}>
-                <Image floated='left' verticalalign='top' src={Dog1} size='medium' rounded />
+                <Image floated='left' verticalalign='top'  size='medium' rounded />
               </Grid.Column>
            
               <Grid.Column floated='left' width={12}>
                 <Card.Header> 
-                    <Header size='huge'> Appa  
-                    <Header.Subheader> Puerto Morelos, México </Header.Subheader> 
+                    <Header size='huge'> {data.placardPet.petName}
+                    <Header.Subheader> {data.location} </Header.Subheader> 
                     </Header> 
                 </Card.Header>
                 
                 <Card.Description>
                     <Segment padded vertical>
-                    <strong> Species: </strong> Chow Chow
+                    <strong> Species: </strong> {data.placardPet.petSpecies}
                     </Segment>
                     <Segment padded vertical>
-                    <strong> Gender: </strong>  Male
+                    <strong> Gender: </strong>  {data.placardPet.petGender}
                     </Segment>
                     <Segment padded vertical>
-                    <strong> Description: </strong>  White dog. Very friendly, if seen please keep him away from the street, he's scared of cars.
+                    <strong> Description: </strong>  {data.placardPet.petColor}. {data.placardPet.petDesc}
                     </Segment>
                     <Segment padded vertical>
-                    <strong> Date I was last seen: </strong>  12/04/2022
+                    <strong> Date I was last seen: </strong>  {data.placardPet.petDateLF}
                     </Segment>
                     <Segment padded vertical>
                     <strong> Reward: </strong>  10,000 mxn
@@ -59,7 +83,7 @@ const IndividualPlacard = () => (
                </Grid.Column>
 
                 <Grid.Column floated='right' width={1}>
-                    <button className="right floated ui negative button"> Lost </button>
+                    <button className="right floated ui negative button"> {data.placardPet.petStatus} </button>
                 </Grid.Column>
               </Card.Content>
 
@@ -74,6 +98,7 @@ const IndividualPlacard = () => (
                 <EmailModal/>
               </Card.Content>
           </Card>
+        
           </Grid.Column>
         </Grid.Row>
 
@@ -86,6 +111,7 @@ const IndividualPlacard = () => (
       </Grid>
     </Container>
   </div>
-)
+</>  
+)};
 
 export default IndividualPlacard;
