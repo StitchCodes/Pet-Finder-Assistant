39f67a825c98c5a7fc307d0cde2e9acaf05abf2d
StitchCodes <coderstitch@gmail.com>
2022-05-11 23:08:54 -0500

    Heroku Deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,9 @@
 // Create mongoose connection
 const mongoose = require("mongoose");
 
-// mongodb+srv://atlasUser:atlasSecPwd@cluster0.l2m6z.mongodb.net/pawtrollerDB?retryWrites=true&w=majority
-// mongodb://localhost:27017/pet-finder-assistant
+
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb+srv://atlasUser:atlasSecPwd@cluster0.l2m6z.mongodb.net/pawtrollerDB?retryWrites=true&w=majority',
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/pet-finder-assistant',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
