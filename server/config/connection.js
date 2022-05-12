95e4c9ae5eb051fd12d7b06ba413d9e2a91823b0
StitchCodes <coderstitch@gmail.com>
2022-05-11 22:48:50 -0500

    Seeded remote Server attempting heroku push

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,10 @@
 // Create mongoose connection
 const mongoose = require("mongoose");
 
-// "mongodb+srv://atlasUser:atlasSecPwd@cluster0.l2m6z.mongodb.net/pawtrollerDB?retryWrites=true&w=majority"
+// mongodb+srv://atlasUser:atlasSecPwd@cluster0.l2m6z.mongodb.net/pawtrollerDB?retryWrites=true&w=majority
+// mongodb://localhost:27017/pet-finder-assistant
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/pet-finder-assistant',
+  process.env.MONGODB_URI || 'mongodb+srv://atlasUser:atlasSecPwd@cluster0.l2m6z.mongodb.net/pawtrollerDB?retryWrites=true&w=majority',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
