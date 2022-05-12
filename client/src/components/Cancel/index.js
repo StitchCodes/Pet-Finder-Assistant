dc05f69f2b03ffe5ac420b4ac3a25224c5050947
ernest1589 <87840643+ernest1589@users.noreply.github.com>
2022-05-12 00:46:27 -0500

    fix cancel for stripe

--- client/src/components/Cancel/index.js
+++ client/src/components/Cancel/index.js
@@ -1,8 +1,15 @@
+import { Container } from "semantic-ui-react";
+
 const Cancel = () => {
   return (
     <div>
-      <h1>Cancel</h1>
-      <h2>Your payment was canceled.</h2>
+      <Container
+        textAlign="center"
+        style={{ marginTop: "8em", marginBottom: "45rem" }}
+      >
+        <h1>Cancel</h1>
+        <h2>Your payment was canceled.</h2>
+      </Container>
     </div>
   );
 };
